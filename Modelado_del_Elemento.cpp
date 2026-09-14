3a08f929054eb06566b85cc57bfec55920374040
FMartinez1207 <00191726@uca.edu.sv>
2026-09-14 11:36:32 -0600

    feat: agregar struct Elemento

--- Modelado_del_Elemento.cpp
+++ Modelado_del_Elemento.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 
+struct Elemento{
+    int codigoElemento;
+    std::string nombreElemento;
+    float longitud;
+    float cargas[3];
+    float capacidad[10];
+    float factorUtilizacion;
+    float estadoSeguridad;
+}
+;
 int main(){
     return 0;
 }
