40014bb4f7188cbe7a9291d6127c472cc64158ff
FMartinez1207 <00191726@uca.edu.sv>
2026-09-14 11:59:52 -0600

    feat: agregar funcion registrarElemento

--- Modelado_del_Elemento.cpp
+++ Modelado_del_Elemento.cpp
@@ -5,11 +5,41 @@ struct Elemento{
     std::string nombreElemento;
     float longitud;
     float cargas[3];
-    float capacidad[10];
+    int capacidad[10];
     float factorUtilizacion;
     float estadoSeguridad;
-}
-;
+};
+
+void registrarElemento(Elemento &elemento);
+
 int main(){
+    Elemento elemento;
+
+    registrarElemento(elemento);
+
     return 0;
+}
+
+void registrarElemento(Elemento &elemento){
+    int n;
+
+    std::cout<<"\nIngrese los datos del elemento\n\n";
+    std::cout<<"Codigo: ";
+    std::cin>>elemento.codigoElemento;
+    std::cout<<std::endl;
+    std::cout<<"Nombre: ";
+    std::cin>>elemento.nombreElemento;
+    std::cout<<std::endl;
+    std::cout<<"Longitud: ";
+    std::cin>>elemento.longitud;
+    std::cout<<std::endl;
+    
+    for(int i = 0; i < 3; i++){
+        std::cout<<"Carga "<<(i + 1)<<": ";
+        std::cin>>elemento.cargas[i];
+        std::cout<<std::endl;
+    }
+
+    std::cout<<"Capacidad maxima: ";
+    std::cin>>elemento.capacidad[n];
 }
